18ce6f7ecf74a97798f306759097081f36c02ec7
Christian <christianlehnhoff@gmail.com>
2019-03-09 12:24:27 +0100

    Add basic function and basic tests

---
+++ Main_Maze_Solver/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Main_Maze_Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maze = new char[,] {
+            { 's', '*', '*',},
+            { '*', 'x', '*',},
+            { '*', '*', 'e', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+
+
+            Implementation(maze);
+
+            Console.WriteLine("--- end ---");
+            Console.ReadKey();
+        }
+
+        public static void Implementation(char[,] myMaze)
+        {
+            /*
+            if (!isMazeAsExpected.isExpectedCharacters(myMaze))
+                throw new ArgumentException(errorMessage_InvalidCharactes);
+
+            if (!isMazeAsExpected.isOneStart(myMaze))
+                throw new ArgumentException(errorMessage_MoreStartChars);
+
+            if (!isMazeAsExpected.isOneEnd(myMaze))
+                throw new ArgumentException(errorMessage_MoreEndChars);
+            */
+
+
+            //Maze.printUpdatedMaze(myMaze);
+            //Console.WriteLine();
+
+            //int[] ActualPosition = get.getPointStart(myMaze);
+            //List<int[]> LastPosition = new List<int[]>();
+            //LastPosition.Add(get.getPointStart(myMaze));
+            //int[] start = get.getPointStart(myMaze);
+            //int[] end = get.getPointEnd(myMaze);
+            //string[] myCompas = Compas.Update(start, end);
+            //bool foundEnd = false;
+            ////List<int[]> logPositions = new List<int[]>();
+
+            //myMaze = Maze.DeleteDeadEnds(myMaze);
+            //Maze.printUpdatedMaze(myMaze);
+            //Console.WriteLine();
+
+            ////logPositions.Add(start);
+            //while (!foundEnd)
+            //{
+            //    // update compas
+            //    //myCompas = Compas.Update(start, end);
+
+            //    // save actual step into previeus step
+            //    LastPosition.Add(ActualPosition);//Position.updateLastPointPosition();
+
+            //    // move
+            //    ActualPosition = Position.moveToNewPosition(myCompas, ActualPosition, LastPosition, myMaze, end);
+            //    if (ActualPosition[0] > 0 || ActualPosition[1] > 0)
+            //        myMaze[ActualPosition[0], ActualPosition[1]] = myMaze[ActualPosition[0], ActualPosition[1]] == 'e' ? 'e' : ',';
+
+            //    // log position
+            //    //logPositions.Add(ActualPosition);
+
+            //    //read if im in end
+            //    foundEnd = get.getFoundEnd(ActualPosition, end);
+            //}
+            ////logPositions.Add(end);
+            //if (!foundDeadEnd)
+            //    Maze.printUpdatedMaze(myMaze);
+            //else
+            //    Console.WriteLine("Maze without solution");
+
+        }
+    }
+}
---
+++ TestMazeSolver/Test_Get.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestMazeSolver
+{
+   [TestClass]
+    public class Test_Get
+    {
+        [TestMethod]
+        public void Test_Get_Up()
+        {
+            var maze = new char[,] {
+            { '1', '2', '3',},
+            { '4', '5', '6',},
+            { '7', '8', '9', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+            var result = MazeSolver.Get.Up(actual_Position, maze);
+
+            Assert.IsTrue('2' == result);
+        }
+
+        [TestMethod]
+        public void Test_Get_Down()
+        {
+            var maze = new char[,] {
+            { '1', '2', '3',},
+            { '4', '5', '6',},
+            { '7', '8', '9', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+            var result = MazeSolver.Get.Down(actual_Position, maze);
+
+            Assert.IsTrue('8' == result);
+        }
+
+        [TestMethod]
+        public void Test_Get_Right()
+        {
+            var maze = new char[,] {
+            { '1', '2', '3',},
+            { '4', '5', '6',},
+            { '7', '8', '9', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+            var result = MazeSolver.Get.Right(actual_Position, maze);
+
+            Assert.IsTrue('6' == result);
+
+        }
+
+        [TestMethod]
+        public void Test_Get_Left()
+        {
+            var maze = new char[,] {
+            { '1', '2', '3',},
+            { '4', '5', '6',},
+            { '7', '8', '9', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+            var result = MazeSolver.Get.Left(actual_Position, maze);
+
+            Assert.IsTrue('4' == result);
+        }
+
+        [TestMethod]
+        public void Test_Get_Down_Right()
+        {
+            var maze = new char[,] {
+            { '1', '2', '3',},
+            { '4', '5', '6',},
+            { '7', '8', '9', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+            var result = MazeSolver.Get.DownRight(actual_Position, maze);
+
+            Assert.IsTrue('9' == result);
+        }
+
+        [TestMethod]
+        public void Test_Get_Down_Left()
+        {
+            var maze = new char[,] {
+            { '1', '2', '3',},
+            { '4', '5', '6',},
+            { '7', '8', '9', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+            var result = MazeSolver.Get.DownLeft(actual_Position, maze);
+
+            Assert.IsTrue('7' == result);
+        }
+
+        [TestMethod]
+        public void Test_Get_Up_Right()
+        {
+            var maze = new char[,] {
+            { '1', '2', '3',},
+            { '4', '5', '6',},
+            { '7', '8', '9', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+            var result = MazeSolver.Get.UpRight(actual_Position, maze);
+
+            Assert.IsTrue('3' == result);
+        }
+
+        [TestMethod]
+        public void Test_Get_Up_Left()
+        {
+            var maze = new char[,] {
+            { '1', '2', '3',},
+            { '4', '5', '6',},
+            { '7', '8', '9', }
+            };
+
+            int[] actual_Position = new int[] { 1, 1 };
+
+            var result = MazeSolver.Get.UpLeft(actual_Position, maze);
+
+            Assert.IsTrue('1' == result);
+        }
+    }
+
+}
