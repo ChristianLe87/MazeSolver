0e98c77bc35b94dc653caa7210a2a480d1be6143
Christian <christianlehnhoff@gmail.com>
2019-06-27 22:02:10 +0200

    changes

---
+++ Mono_MazeSolver_MacOS/Game1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Mono_MazeSolver_MacOS
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D jugador;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            graphics.PreferredBackBufferWidth = 180;
+            graphics.PreferredBackBufferHeight = 120;
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            jugador = Content.Load<Texture2D>("Path");
+
+
+            //TODO: use this.Content to load your game content here 
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // For Mobile devices, this logic will close the Game when the Back button is pressed
+            // Exit() is obsolete on iOS
+            #if !__IOS__ &&  !__TVOS__
+            if (GamePad.GetState (PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape)) 
+                Exit ();
+            #endif
+            
+            // TODO: Add your update logic here
+            
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            graphics.GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            //TODO: Add your drawing code here
+            spriteBatch.Begin();
+            spriteBatch.Draw(jugador, Vector2.Zero, Color.White); // Color.White es opcional
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- Shared/Maze.cs
+++ Shared/Maze.cs
@@ -1,10 +1,43 @@
 ﻿using System;
-namespace Shared
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Mono_MazeSolver_MacOS
 {
     public class Maze
     {
-        public Maze()
+        List<List<string>> fileContent;
+        Texture2D Texture_NotWall;
+        Texture2D Texture_Wall;
+        Texture2D Texture_Path;
+        Texture2D Texture_Start;
+        Texture2D Texture_End;
+        Rectangle rectangle = new Rectangle(0, 0, 20, 20);
+
+
+        public Maze(List<List<string>> fileContent)
         {
+            this.fileContent = fileContent;
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            foreach (var line in fileContent)
+            {
+                foreach(var element in line)
+                {
+                    if(element == " ")
+                    {
+                        spriteBatch.Draw(Texture_NotWall, rectangle, Color.White);
+                    }
+                    else
+                    {
+                        spriteBatch.Draw(Texture_Wall, rectangle, Color.White);
+                    }
+
+                }
+            }
         }
     }
 }
