57051f9e48ac2e6f5ebb8b6882056e4f6f3de600
ChristianLe87 <christianle87@icloud.com>
2024-06-25 10:48:23 +0200

    Apply Tasks

--- Shared/Maze.cs
+++ Shared/Maze.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
@@ -9,9 +11,9 @@ namespace Shared
     public class Maze
     {
         List<List<MazeTile>> mazeTiles;
+        List<List<MazeTile>> newMazeTiles;
 
-
-
+        private int count;
         public Maze(ContentManager Content)
         {
             mazeTiles = new List<List<MazeTile>>();
@@ -34,7 +36,8 @@ namespace Shared
 
         }
 
-
+        private bool gameRunnung = false;
+        private bool isTasking = false;
         public void Update(MouseState mouseState, KeyboardState keyboardState)
         {
             foreach (var i in mazeTiles)
@@ -46,48 +49,29 @@ namespace Shared
             }
 
             // calculate final maze
+
             if (keyboardState.IsKeyDown(Keys.C) == true)
             {
-                List<List<string>> myCalculatedMaze = new List<List<string>>();
-
-                // Clean tiles
-                for (var row = 0; row < mazeTiles.Count; row++)
+                gameRunnung = true;
+            }
+            
+            
+            if (gameRunnung)
+            {
+                if (isTasking == false && count == 60)
                 {
-                    for (var element = 0; element < mazeTiles.Count; element++)
+                    count = 0;
+                    Task myTask = new Task(() =>
                     {
-                        if (mazeTiles[row][element].Name == "*")
-                        {
-                            mazeTiles[row][element].Name = " ";
-                        }
-                    }
+                        isTasking = true;
+                        CalculateFinalMaze();
+                        isTasking = false;
+                    });
+                    myTask.Start();
                 }
-
-                for (var row = 0; row < mazeTiles.Count; row++)
+                else
                 {
-                    List<string> rows = new List<string>();
-                    foreach (var element in mazeTiles[row])
-                    {
-                        rows.Add(element.Name);
-                    }
-                    myCalculatedMaze.Add(rows);
-                }
-
-                var maze = new MazeSolver.Maze(myCalculatedMaze);
-                MazeSolver.Tools.FillWithNumbers(maze);
-                MazeSolver.Tools.GetShortestPath(maze);
-                MazeSolver.Tools.CleanMaze(maze);
-                MazeSolver.Tools.PrintMaze(maze);
-
-                // update tiles
-                for (var row = 0; row < mazeTiles.Count; row++)
-                {
-                    for (var element = 0; element < mazeTiles.Count; element++)
-                    {
-                        if (myCalculatedMaze[row][element] == "*")
-                        {
-                            mazeTiles[row][element].Name = "*";
-                        }
-                    }
+                    count ++;
                 }
             }
         }
@@ -102,6 +86,60 @@ namespace Shared
                     item.Draw(spriteBatch);
                 }
             }
+        }
+
+        private void CalculateFinalMaze()
+        {
+            List<List<string>> myCalculatedMaze = new List<List<string>>();
+
+            // Clean tiles
+            newMazeTiles = mazeTiles.ToList();
+
+            
+            // Clean tiles
+            for (var row = 0; row < newMazeTiles.Count; row++)
+            {
+                for (var element = 0; element < newMazeTiles.Count; element++)
+                {
+                    if (newMazeTiles[row][element].Name == "*")
+                    {
+                        newMazeTiles[row][element].Name = " ";
+                    }
+                }
+            }
+            
+            
+
+            for (var row = 0; row < newMazeTiles.Count; row++)
+            {
+                List<string> rows = new List<string>();
+                foreach (var element in newMazeTiles[row])
+                {
+                    rows.Add(element.Name);
+                }
+                myCalculatedMaze.Add(rows);
+            }
+
+            var maze = new MazeSolver.Maze(myCalculatedMaze);
+            MazeSolver.Tools.FillWithNumbers(maze);
+            MazeSolver.Tools.GetShortestPath(maze);
+            MazeSolver.Tools.CleanMaze(maze);
+            MazeSolver.Tools.PrintMaze(maze);
+
+            // update tiles
+            for (var row = 0; row < newMazeTiles.Count; row++)
+            {
+                for (var element = 0; element < newMazeTiles.Count; element++)
+                {
+                    if (myCalculatedMaze[row][element] == "*")
+                    {
+                        newMazeTiles[row][element].Name = "*";
+                    }
+                }
+            }
+
+            mazeTiles = newMazeTiles.ToList();
+
         }
     }
 }
